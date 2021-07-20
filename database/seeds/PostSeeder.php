d159cc77fffd8605641cc68b3a5f3e864a37dd86
Cargalher <cargalher@gmail.com>
2021-07-21 00:39:51 +0200

    for loop created and fakers & migrated

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,7 +1,7 @@
 <?php
 use App\Post;
 use Illuminate\Database\Seeder;
-
+use Faker\Generator as Faker;
 class PostSeeder extends Seeder
 {
     /**
@@ -13,7 +13,13 @@ class PostSeeder extends Seeder
     {
         for ($i = 0; $i < 10; $i++) {
             $post = new Post();
-            $post->title
+            $post->title = $faker-> sentence(3);
+            $post->author = $faker-> name();
+            $post->post_date = $faker-> dateTimeThisYear();
+            $post->post_content = $faker-> text(500);
+            $post->image = $faker-> imageUrl(640, 480, 'Posts', true, $post->title);
+            $post->published = $faker-> boolean();
+            $post->save();
         }
     }
 }
