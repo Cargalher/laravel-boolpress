54c70f8849e10bd67cd531c8ff020fa505e28f24
Cargalher <cargalher@gmail.com>
2021-07-21 15:53:56 +0200

    folder post and file index for admin_table started

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="table-responsive">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Image</th>
+                <th>Author</th>
+                <th>Date</th>
+                <th>Content</th>
+                <th>Published</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+
+
+@endsection
