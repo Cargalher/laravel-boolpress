270c8f7da9b0f6c840e2372a8b1826135228d401
Cargalher <cargalher@gmail.com>
2021-07-22 18:20:26 +0200

    created page controler & added functions for home, about and contact

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', 'PageController@index');
+Route::get('about', 'PageController@about');
+Route::get('contacts', 'PageController@contacts');
+
+
+Route::get('posts', 'PostController@index')->name('posts.index');
+Route::get('posts/{post}', 'PostController@show')->name('posts.show');
+
 /*guest Routes*/
 Route::get('/', function () {
     return view('guest.welcome');
