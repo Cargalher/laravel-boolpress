b3600f2376520722b9283859940e02107e76c436
Cargalher <cargalher@gmail.com>
2021-07-23 19:08:10 +0200

    class to send email and email template

---
+++ resources/views/emails/contacts.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Email</title>
+</head>
+<body>
+    <h1>Hello! You have got a new message</h1>
+    <p>Message from: "email"</p>
+    <p>Message: 
+
+    
+    </p>
+</body>
+</html>
